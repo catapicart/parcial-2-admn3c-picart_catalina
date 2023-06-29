eb9251852afec9e9969a5a44d969740b2657373d
catapicart <catapicart@gmail.com>
2023-06-29 12:00:14 -0300

    change navbar

--- clasecli/src/App.vue
+++ clasecli/src/App.vue
@@ -2,39 +2,19 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
+      color="secondary"
       dark
     >
-      <div class="d-flex align-center">
+      <nav class="d-flex align-center justify-center">
         <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          alt="Superquiz Logo"
+          src="./assets/superquiz-logo.png"
           transition="scale-transition"
           width="40"
         />
+      </nav>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <!-- <v-spacer></v-spacer> -->
     </v-app-bar>
 
     <v-main>
